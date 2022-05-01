1b067889222b478be65791bbebd6f4fbf92d5a8b
alphadlplus <82231865+alphadlplus@users.noreply.github.com>
2022-05-01 17:03:30 +0430

    Update renamer.py

--- tobrot/helper_funcs/renamer.py
+++ tobrot/helper_funcs/renamer.py
@@ -1,11 +1,15 @@
 import  re
 
 def Renamer_TG(name):
-    orgname = name.replace("HEVC", "").replace("H264","").replace("x264","").replace("[","").replace("MX]","").replace(".in","").replace(".ph","").replace(".li","")
+    orgname = name.replace(".HEVC-", "").replace(".H264-","").replace(".x264-","").replace(".HEVC", "").replace(".H264","").replace(".x264","").replace("[","").replace(".MX]","").replace(".in","").replace(".ph","").replace(".li","").replace("HEVC", "").replace("H264","").replace("x264","")
+    orgname = orgname.replace(".AlphaDL", "")
     regex = re.search(r"(\..{3,4})$", orgname)
     orgname = orgname.replace(regex.group(1),".AlphaDL%s"%regex.group(1))
+    if len(orgname) > 64:
+        orgname = orgname.replace("AlphaDL", "AD")
     regex = re.search(r"[Ss]\d{1,2}.?[Ee]\d{1,2}(.+)\.\d{3,4}p", orgname)
-    orgname = orgname.replace(regex.group(1),"")
+    if re.match(r"[Ss]\d{1,2}.?[Ee]\d{1,2}(.+)\.\d{3,4}p", orgname):
+        orgname = orgname.replace(regex.group(1),"")
     return orgname
 
 def Renamer_GD(name):
